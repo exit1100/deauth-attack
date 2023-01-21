00051942f058faddd9d52d81f32c4d71a4b49959
exit1100 <85146195+exit1100@users.noreply.github.com>
2023-01-22 00:02:49 +0900

    Revert "Revert "[YEJUN] Fake Beacon Flood""
    This reverts commit bf0bb8a0af254afcdb99fbb9cf002e375ddee26e.

--- src/deauth-attack-whitelist/beacon.cpp
+++ src/deauth-attack-whitelist/beacon.cpp
@@ -3,31 +3,30 @@
 #include <arpa/inet.h>
 #define NULL 0x00
 
-
 struct radiotap_header {
-    uint8_t     version;     /* set to 0 */
-    uint8_t     pad;
-    uint16_t    len;         /* entire length */
-    uint32_t    present;     /* fields present */
-    uint8_t     dummy[3];
+    uint8_t     version = 0x00;     /* set to 0 */
+    uint8_t     pad = 0x00;
+    uint16_t    len = 0x000b;         /* entire length */
+    uint32_t    present = 0x00028000;     /* fields present */
+    uint8_t     dummy[3] = {0,};
 } __attribute__((__packed__));
 
-struct beacon_header{
-    uint16_t frame_control;
-    uint16_t duration_id;
-    uint8_t dhost[6];  //목적지 주소
-    uint8_t shost[6];  //출발지 주소
-    uint8_t bssid[6];
-    uint16_t squence_control;
+struct IEEE_802dot11 {
+    uint16_t frame_control = 0x00c0;
+    uint16_t duration_id = 0x0000;
+    uint8_t dhost[6] = {0,};  //목적지 주소
+    uint8_t shost[6] = {0,};  //출발지 주소
+    uint8_t bssid[6] = {0,};
+    uint16_t squence_control = 0x0000;
 } __attribute__ ((__packed__));
 
 struct fixed_parameters{
-    uint16_t reason_code;
+    uint16_t reason_code = 0x0007;
 } __attribute__ ((__packed__));
 
-struct fake_beacon{
+struct beacon_frame{
     struct radiotap_header radiotap;
-    struct beacon_header becon;
+    struct IEEE_802dot11 becon;
     struct fixed_parameters fixed;
 } __attribute__ ((__packed__));
 
@@ -45,7 +44,7 @@ struct multiargs{
     char* white_list;
 };
 
-struct fake_beacon create_beacon_frame();
+struct beacon_frame create_beacon_frame();
 int dump_radiotap(struct radiotap_header *radiotap_header){
     unsigned int len = radiotap_header->len;
     //printf("[Radiotap Length] : %d\n",len);
